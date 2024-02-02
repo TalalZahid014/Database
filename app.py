4f5b9a3cf61ab2d9a29728b629e458da23d0f1f3
TalalZahid014 <talalzahid012@gmail.com>
2024-02-02 21:19:56 +0500

    view info

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ import psycopg2
 app = Flask(__name__)
 
 def get_db_connection():
-    return psycopg2.connect(   
-    host ='localhost',
-    port ='5432',
-    dbname = 'Fullstack',
-    user ='postgres',
-    password ='12345')
+  return psycopg2.connect(   
+  host ='localhost',
+  port ='5432',
+  dbname = 'Fullstack',
+  user ='postgres',
+  password ='12345')
 
 conn = get_db_connection()
 
@@ -23,26 +23,34 @@ def index():
     conn.close()
     return render_template('index.html', users = users)
     
-
+@app.route('/user/<int:user_id>')
+def view_user(user_id):
+# Display details of a single user
+  conn = get_db_connection()
+  cursor = conn.cursor()
+  cursor.execute("SELECT * FROM data WHERE id = %s", (user_id,))
+  user = cursor.fetchone()
+  cursor.close()
+  conn.close()
+  return render_template('view_user.html', user=user)
 
 @app.route('/submit',methods=['POST'])
 def submit():
-    username = request.form['user_name']
-    firstname = request.form['name_first']
-    lastname = request.form['name_last']
-    password = request.form['pass_word']
-
+  username = request.form['user_name']
+  firstname = request.form['name_first']
+  lastname = request.form['name_last']
+  password = request.form['pass_word']
     
 
-    cursor = conn.cursor()
+  cursor = conn.cursor()
 
-    cursor.execute("INSERT INTO data (user_name,name_first,name_last,pass_word) values (%s,%s,%s,%s)",(username,firstname,lastname,password))
+  cursor.execute("INSERT INTO data (user_name,name_first,name_last,pass_word) values (%s,%s,%s,%s)",(username,firstname,lastname,password))
 
-    conn.commit()
+  conn.commit()
 
-    cursor.close()
-    conn.close()
+  cursor.close()
+  conn.close()
 
-    return redirect('/')
+  return redirect('/')
 if __name__ == '__main__':
-    app.run(debug=True)
+  app.run(debug=True)
