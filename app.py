6a5ab9588cedaa35f72180f189c8f2ca0b8b77c5
TalalZahid014 <talalzahid012@gmail.com>
2024-02-12 01:17:44 +0500

    "aligned edit user page"

--- app.py
+++ app.py
@@ -33,6 +33,7 @@ def view_user(user_id):
   cursor.close()
   conn.close()
   return render_template('view_user.html', user=user)
+
 @app.route('/edit_user/<int:user_id>', methods=['GET', 'POST'])
 def edit_user(user_id):
   # Edit an existing user
@@ -41,29 +42,31 @@ def edit_user(user_id):
   cursor.execute("SELECT * FROM data WHERE id = %s", (user_id,))
   user = cursor.fetchone()
   if request.method == 'POST':
-      new_username = request.form['user_name']
-      new_firstname = request.form['name_first']
-      new_lastname = request.form['name_last']
-      new_password = request.form['pass_word']
-      cursor.execute("UPDATE data SET user_name = %s, name_first = %s, name_last = %s, pass_word = %s, WHERE id = %s",(new_username, new_firstname, new_lastname, new_password, user_id))
-      conn.commit()
-      cursor.close()
-      conn.close()
-      return redirect(url_for('index'))
-
-  cursor.close()
-  conn.close()
-  return render_template('edit_user.html', user=user)
-@app.route('/delete_user/<int:user_id>')
-def delete_user(user_id):
-    # Delete an existing user
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute("DELETE FROM data WHERE id = %s", (user_id,))
+    new_username = request.form['user_name']
+    new_firstname = request.form['name_first']
+    new_lastname = request.form['name_last']
+    new_password = request.form['pass_word']
+    cursor.execute("UPDATE data SET user_name = %s, name_first = %s, name_last = %s, pass_word = %s, WHERE id = %s",(new_username, new_firstname, new_lastname, new_password, user_id))
     conn.commit()
     cursor.close()
     conn.close()
     return redirect(url_for('index'))
+
+  cursor.close()
+  conn.close()
+  return render_template('edit_user.html', user=user)
+
+@app.route('/delete_user/<int:user_id>')
+def delete_user(user_id):
+  # Delete an existing user
+  conn = get_db_connection()
+  cursor = conn.cursor()
+  cursor.execute("DELETE FROM data WHERE id = %s", (user_id,))
+  conn.commit()
+  cursor.close()
+  conn.close()
+  return redirect(url_for('index'))
+
 @app.route('/submit',methods=['POST'])
 def submit():
   username = request.form['user_name']
